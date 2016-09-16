b1d7734a91a570aa781eb19d2b9e80d65a9bf9a1
ZackExplosion <cstony0917@gmail.com>
2016-09-17 04:55:00 +0800

    set camerapreview degree

--- app/src/main/java/com/explosion/zack/facebooklivestreamwithbluetoothaudio/CameraPreview.java
+++ app/src/main/java/com/explosion/zack/facebooklivestreamwithbluetoothaudio/CameraPreview.java
@@ -18,6 +18,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         super(context);
         mCamera = camera;
 
+        mCamera.setDisplayOrientation(90);
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
         mHolder = getHolder();
